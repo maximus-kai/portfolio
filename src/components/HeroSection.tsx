f14fa665d9e56f258ab218895d0fe8b538cbdf8a
Maximus <tomiwadairo@gmail.com>
2024-05-15 11:21:06 +0100

    First commit

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <>
+    <div className="text-white mb-4 text-6 xl font-extrabold">
+        LOREM I'P SUM
+      </div>
+      <p className='text-[#adb7be] text-5xl font-extrabold'>
+        Loem ipsum dolor sit amet, consectetur adipisicing elit. Ratione officiis explicabo, esse iusto ullam quis, facilis voluptates rem similique sed ab accusantium maxime. Voluptate mollitia quod placeat! Accusamus, sint alias!
+      </p>
+    </>
+  )
+}
