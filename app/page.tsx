39196017a4efd8c11c369c2b2c7541b714b3a0dc
Maximus <tomiwadairo@gmail.com>
2024-05-15 12:03:14 +0100

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import HeroSection from "../src/components/HeroSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
-      <HeroSection />
+      <HeroSection/>
     </main>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Hero() {
   return (
     <>
-    <div className="text-white mb-4 text-6 xl font-extrabold">
-        LOREM I'P SUM
-      </div>
+    <h1 className="text-white mb-4 text-6 xl font-extrabold">
+        LOREM IP SUM
+      </h1>
       <p className='text-[#adb7be] text-5xl font-extrabold'>
         Loem ipsum dolor sit amet, consectetur adipisicing elit. Ratione officiis explicabo, esse iusto ullam quis, facilis voluptates rem similique sed ab accusantium maxime. Voluptate mollitia quod placeat! Accusamus, sint alias!
       </p>
