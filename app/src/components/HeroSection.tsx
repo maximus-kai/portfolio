90393fae81ecb7051e22f8a3bb3b0f81da136da3
Maximus <tomiwadairo@gmail.com>
2024-05-15 13:09:05 +0100

    updated the lorem desc

--- app/src/components/HeroSection.tsx
+++ app/src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
     <h1 className="text-white text-6xl font-extrabold mb-4 ">
         LOREM IP SUMM
       </h1>
-      <p className='text-white text-2xl font-medium'>
+      <p className='text-[#adb7b8] text-2xl font-medium'>
         Loem ipsum dolor sit amet, consectetur adipisicing elit. Ratione officiis explicabo, esse iusto ullam quis, facilis voluptates rem similique sed ab accusantium maxime. Voluptate mollitia quod placeat! Accusamus, sint alias!
       </p>
     </>
