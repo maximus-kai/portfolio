714dfa7ebd37d68e7de751ccf3bdf686b4e0d81a
Maximus <tomiwadairo@gmail.com>
2024-05-16 05:29:46 +0100

    centered text on small screet

--- app/src/components/HeroSection.tsx
+++ app/src/components/HeroSection.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 export default function Hero() {
   return (
     <section>
-      <div className='grid grid-cols-1 lg:grid-cols-12'>
-        <div className='col-span-7 place-self-center'>   
-    <h1 className="text-white text-4xl lg:text-6xl font-extrabold mb-4 ">
+      <div className='grid grid-cols-1 sm:grid-cols-12'>
+        <div className='col-span-7 place-self-center text-center sm:text-left'>   
+    <h1 className="text-white text-4xl lg:text-6xl sm:text-5xl font-extrabold mb-4 ">
       HORNS OF A DILEMMA
       </h1>
       <p className='text-[#adb7b8] text-lg lg:text-xl mb-6 font-medium'>
