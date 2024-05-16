175548f61ad14c7c5f089c09a484bcdfe7e6e239
Maximus <tomiwadairo@gmail.com>
2024-05-16 04:18:07 +0100

    added image

--- app/src/components/HeroSection.tsx
+++ app/src/components/HeroSection.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
-
+import Image from 'next/image'
 export default function Hero() {
   return (
-    <>
-    <h1 className="text-white text-6xl font-extrabold mb-4 ">
+    <section>
+      <div className='grid grid-cols-1 lg:grid-cols-12'>
+        <div className='col-span-7 place-self-center'>   
+    <h1 className="text-white text-4xl lg:text-6xl font-extrabold mb-4 ">
         LOREM IP SUMM
       </h1>
-      <p className='text-[#adb7b8] text-2xl font-medium'>
+      <p className='text-[#adb7b8] text-lg lg:text-xl font-medium'>
         Loem ipsum dolor sit amet, consectetur adipisicing elit. Ratione officiis explicabo, esse iusto ullam quis, facilis voluptates rem similique sed ab accusantium maxime. Voluptate mollitia quod placeat! Accusamus, sint alias!
       </p>
-    </>
+        </div>
+        <div className='col-span-5'>
+          <div className='mt-5 rounded-full aspect-square bg-[#181818] w-[500px] h-[400px] relative'>
+            
+        </div>
+            </div>
+      </div>
+    </section>
   )
 }
