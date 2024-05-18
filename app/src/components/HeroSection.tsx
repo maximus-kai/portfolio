fba1a41e5163f3cb9d043f64f78b5859db15f9f7
Maximus <tomiwadairo@gmail.com>
2024-05-18 11:06:38 +0100

    updated text and button

--- app/src/components/HeroSection.tsx
+++ app/src/components/HeroSection.tsx
@@ -19,10 +19,12 @@ export default function Hero() {
           </p>
           <div>
             <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-orange-500 hover:bg-slate-200 text-white'>
-              To Be
+              Hire me
             </button>
-            <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-transparent border border-white text-white hover:bg-slate-800 mt-3'>
-              Not To Be             </button>
+            <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-orange-500  text-white hover:bg-slate-800 mt-3'>
+              <span className='block bg-[#121212] hover:bg-slate-700 rounded-full px-5 py-2'>Download CV</span>
+              
+            </button>
           </div>
         </div>
         <div className='col-span-5 mt-4 lg:mt-0 place-self-center'>
