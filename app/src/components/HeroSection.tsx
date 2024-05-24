320c55263a1f0c8bbdf41fb48b1fe6ffe632458d
Maximus <tomiwadairo@gmail.com>
2024-05-24 23:54:44 +0100

    made some changes to the english in my hero section

--- app/src/components/HeroSection.tsx
+++ app/src/components/HeroSection.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
       <div className='grid grid-cols-1 sm:grid-cols-12'>
         <div className='col-span-7 place-self-center text-center sm:text-left'>   
     <h1 className="text-white text-4xl lg:text-6xl sm:text-5xl font-extrabold mb-4 ">
-            <span className='text-transparent text-white bg-clip-text bg-gradient-to-r from-blue-500 to-orange-500'>    
+            <span className='text-transparent  bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-orange-500'>    
               Hello, I&apos;m{" "}
               <br/>
             </span> 
