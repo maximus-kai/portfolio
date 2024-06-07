626ad33c7af5c85f08f9c9f719ceb524a05c27e6
Maximus <tomiwadairo@gmail.com>
2024-06-07 21:14:29 +0100

    Project section established

--- app/src/components/ProjectCard.tsx
+++ app/src/components/ProjectCard.tsx
@@ -1,22 +1,55 @@
 import React from 'react'
 import Image from 'next/image'
+import { CodeBracketIcon, EyeIcon } from '@heroicons/react/16/solid';
+import Link from 'next/link';
 
 interface ProjectCardProps{
     ImageUrl: string;
     title: string;
     description: string;
+    gitUrl: string;
+    previewUrl: string;
 }
 
-const ProjectCard:React.FC<ProjectCardProps> = ({ImageUrl,title,description}) => {
+const ProjectCard:React.FC<ProjectCardProps> = ({ImageUrl,title,description,gitUrl,previewUrl}) => {
   return (
-      <div className='border bprder-round-full'>
-         <Image
-              className='absolute transform -translate-x-1/2 bg-cover  left-1/2'
-            src={ImageUrl}
-            alt='project 1'
-            width={500}
-              height={500} />
-          <h1 className=''>{ description}</h1>
+    <div>
+      <div className='relative  group'>
+        <Image
+          className='h-52 md:h-72 bg-cover rounded-t-xl '
+              src={ImageUrl}
+              alt='project 1'
+              width={500}
+          height={500} />
+        <div className='
+        overlay 
+        items-center
+        justify-center
+        absolute
+        top-0
+        left-0
+        w-full
+        h-full
+        bg-[#181818]
+        bg-opacity-0
+        hidden
+        group-hover:flex
+        group-hover:bg-opacity-80
+        transition-all
+        duration-500 '>
+          <Link href={gitUrl} className='h-14 w-14 border-2 relative mr-5 rounded-full border-[#adb7be] hover:border-white'>
+            <CodeBracketIcon className='h-10 w-10 text-[#adb7be] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer hover:text-white'/>
+          </Link>
+          <Link href={previewUrl} className='h-14 w-14 border-2 relative rounded-full border-[#adb7be] hover:border-white'>
+            <EyeIcon className='h-10 w-10 text-[#adb7be] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer hover:text-white'/>
+          </Link>
+
+        </div>
+      </div>
+      <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4" >
+          <h5 className='text-xl font-extrabold mb-2'>{title}</h5>
+          <p className='text-[#adb7be]' >{description}</p>
+      </div>
     </div>
   )
 }
--- app/src/components/ProjectsSection.tsx
+++ app/src/components/ProjectsSection.tsx
@@ -1,11 +1,73 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
 
+const projectsData = [
+    {
+        id: 1,
+        title: 'Project One',
+        description:'First Project',
+        image: '/images/1.jpg',
+        gitUrl: '/',
+        previewUrl:'/',
+        tag: ['All','Web'],
+    },
+    {
+        id: 1,
+        title: 'Project Two',
+        description:'Second Project',
+        image: '/images/2.jpg',
+        gitUrl: '/',
+        previewUrl:'/',
+        tag: ['All','Web'],
+    },
+    {
+        id: 1,
+        title: 'Project Three',
+        description:'Third Project',
+        image: '/images/3.jpg',
+        gitUrl: '/',
+        previewUrl:'/',
+        tag: ['All','Web'],
+    },
+    {
+        id: 1,
+        title: 'Project Four',
+        description:'Forth Project',
+        image: '/images/4.jpg',
+        gitUrl: '/',
+        previewUrl:'/',
+        tag: ['All','Mobile'],
+    },
+    {
+        id: 1,
+        title: 'Project Five',
+        description:'Fifth Project',
+        image: '/images/5.jpg',
+        gitUrl: '/',
+        previewUrl:'/',
+        tag: ['All','Mobile'],
+    },
+    {
+        id: 1,
+        title: 'Project Six',
+        description:'Sixth Project',
+        image: '/images/6.jpg',
+        gitUrl: '/',
+        previewUrl:'/',
+        tag: ['All','Mobile'],
+    },
+]
+
 const ProjectsSection = () => {
     return (
         <>
-    <h2 className='text-4xl font-bold text-white mb-4 mt-4 md:mt-0 text-center'>My Projects</h2>
-    <ProjectCard ImageUrl='/images/1.jpg' title='' description='Description'/>
+            <h2 className='text-4xl  font-bold text-white mb-4 mt-4 md:mt-0 text-center'>My Projects</h2>
+            <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+
+            {
+                projectsData.map((eachProject) => (<ProjectCard gitUrl={eachProject.gitUrl} previewUrl={eachProject.previewUrl } key={eachProject.id} ImageUrl={eachProject.image} title={eachProject.title} description={eachProject.description } />))
+            }
+            </div>
         </>
   )
 }
